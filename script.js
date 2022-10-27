ce37984c6d101221eaaa9fb68a0b6a6cfef04a1b
nakis93 <zjnakis@gmail.com>
2022-10-27 15:19:04 +0300

    Finished the project's first stage (JS only)

--- script.js
+++ script.js
@@ -3,72 +3,100 @@
 const hands = ["rock", "paper", "scissors"];
 let computerSelection;
 let playerSelection;
-
+let playerScore = 0;
+let computerScore = 0;
+const [a, b, c] = hands;
 function getPlayerChoice() {
-  playerSelection = prompt("Choose your weapon!");
-  return playerSelection.toUpperCase();
+  playerSelection = prompt("Choose your weapon!").toUpperCase();
 }
-// getPlayerChoice();
 
 function getComputerChoice() {
-  computerSelection = hands[Math.floor(Math.random() * hands.length)];
-  return computerSelection.toUpperCase();
+  computerSelection =
+    hands[Math.floor(Math.random() * hands.length)].toUpperCase();
 }
-// getComputerChoice();
 
 function playRound() {
   getPlayerChoice();
   getComputerChoice();
+  // console.log(computerSelection);
+  // console.log(playerSelection);
+  // console.log(playerSelection.toUpperCase());
+  // console.log(a);
+  // console.log(a.toUpperCase());
+  // console.log(playerSelection);
+  // console.log(a);
+  if (
+    playerSelection !== a.toUpperCase() &&
+    playerSelection !== b.toUpperCase() &&
+    playerSelection !== c.toUpperCase()
+  ) {
+    alert("Invalid input! Please choose between rock, paper, scissors! ");
+  }
   switch (playerSelection) {
-    case "rock":
+    case "ROCK":
       if (playerSelection === computerSelection)
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection} too. It's a tie!`
+          `You got ${playerSelection} and the computer got ${computerSelection} too. It's a tie! The score is ${playerScore}vs${computerScore}`
         );
-      else if (computerSelection === "paper")
+      else if (computerSelection === "PAPER") {
+        computerScore = computerScore + 1;
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection}. You lose!`
+          `You got ${playerSelection} and the computer got ${computerSelection}. You lose! The score is ${playerScore}vs${computerScore}`
         );
-      else if (computerSelection === "scissors")
+      } else if (computerSelection === "SCISSORS") {
+        playerScore = playerScore + 1;
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection}. You WIN!`
+          `You got ${playerSelection} and the computer got ${computerSelection}. You WIN! The score is ${playerScore}vs${computerScore}`
         );
+      }
 
       break;
 
-    case "paper":
+    case "PAPER":
       if (playerSelection === computerSelection)
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection} too. It's a tie!`
+          `You got ${playerSelection} and the computer got ${computerSelection} too. It's a tie! The score is ${playerScore}vs${computerScore}`
         );
-      else if (computerSelection === "scissors")
+      else if (computerSelection === "SCISSORS") {
+        computerScore = computerScore + 1;
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection}. You lose!`
+          `You got ${playerSelection} and the computer got ${computerSelection}. You lose! The score is ${playerScore}vs${computerScore}`
         );
-      else if (computerSelection === "rock")
+      } else if (computerSelection === "ROCK") {
+        playerScore = playerScore + 1;
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection}. You WIN!`
+          `You got ${playerSelection} and the computer got ${computerSelection}. You WIN! The score is ${playerScore}vs${computerScore}`
         );
+      }
 
       break;
 
-    case "scissors":
+    case "SCISSORS":
       if (playerSelection === computerSelection)
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection} too. It's a tie!`
+          `You got ${playerSelection} and the computer got ${computerSelection} too. It's a tie! The score is ${playerScore}vs${computerScore}`
         );
-      else if (computerSelection === "rock")
+      else if (computerSelection === "ROCK") {
+        computerScore = computerScore + 1;
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection}. You lose!`
+          `You got ${playerSelection} and the computer got ${computerSelection}. You lose! The score is ${playerScore}vs${computerScore}`
         );
-      else if (computerSelection === "paper")
+      } else if (computerSelection === "PAPER") {
+        playerScore = playerScore + 1;
         console.log(
-          `You got ${playerSelection} and the computer got ${computerSelection}. You WIN!`
+          `You got ${playerSelection} and the computer got ${computerSelection}. You WIN! The score is ${playerScore}vs${computerScore}`
         );
+      }
 
       break;
   }
 }
-for (let i = 0; i < 5; i++) {
-  playRound();
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
 }
+game();
+if (playerScore > computerScore) console.log("CONGRATULATIONS! YOU WON!");
+else if (playerScore === computerScore) console.log("IT'S A TIE!");
+else console.log("YOU LOST! THE COMPUTER WON!");
